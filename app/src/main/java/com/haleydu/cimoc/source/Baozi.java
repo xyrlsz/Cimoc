7b05c74c27f3860155f5674397adaedc346a62de
xyrlsz <xyrlsz@outlook.com>
2025-01-24 01:08:29 +0800

    🐞 fix: 包子漫画章节目录顺序问题

--- app/src/main/java/com/haleydu/cimoc/source/Baozi.java
+++ app/src/main/java/com/haleydu/cimoc/source/Baozi.java
@@ -7,26 +7,20 @@ import com.haleydu.cimoc.model.ImageUrl;
 import com.haleydu.cimoc.model.Source;
 import com.haleydu.cimoc.parser.MangaParser;
 import com.haleydu.cimoc.parser.NodeIterator;
-import com.haleydu.cimoc.parser.RegexIterator;
 import com.haleydu.cimoc.parser.SearchIterator;
 import com.haleydu.cimoc.parser.UrlFilter;
 import com.haleydu.cimoc.soup.Node;
 import com.haleydu.cimoc.utils.StringUtils;
 
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import okhttp3.FormBody;
 import okhttp3.Headers;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 
 /**
  * Created by xyrlsz on 2025/01/07.
@@ -36,8 +30,9 @@ public class Baozi extends MangaParser {
 
     public static final int TYPE = 101;
     public static final String DEFAULT_TITLE = "包子漫画";
-//    private static String baseUrl = "https://www.baozimh.com";
-    private static String baseUrl = "https://cn.baozimhcn.com";
+    //    private static String baseUrl = "https://www.baozimh.com";
+    private static final String baseUrl = "https://cn.baozimhcn.com";
+
     public Baozi(Source source) {
         init(source, null);
     }
@@ -49,7 +44,7 @@ public class Baozi extends MangaParser {
     @Override
     public Request getSearchRequest(String keyword, int page) throws UnsupportedEncodingException {
 //        if (page != 1) return null;
-        String url = baseUrl + "/search?q="+keyword;
+        String url = baseUrl + "/search?q=" + keyword;
         return new Request.Builder().url(url).build();
     }
 
@@ -64,14 +59,14 @@ public class Baozi extends MangaParser {
 
                 String cid = node.href(".comics-card__info").split("/")[2];
                 String cover = node.src(".comics-card > a > amp-img");
-                return new Comic(TYPE,cid,title,cover,null,author);
+                return new Comic(TYPE, cid, title, cover, null, author);
             }
         };
     }
 
     @Override
     public String getUrl(String cid) {
-        return baseUrl+"/comic/"+cid;
+        return baseUrl + "/comic/" + cid;
     }
 
     @Override
@@ -81,7 +76,7 @@ public class Baozi extends MangaParser {
 
     @Override
     public Request getInfoRequest(String cid) {
-        String url=baseUrl+"/comic/"+cid;
+        String url = baseUrl + "/comic/" + cid;
         return new Request.Builder().url(url).build();
     }
 
@@ -94,8 +89,8 @@ public class Baozi extends MangaParser {
         String intro = body.text(".comics-detail__desc");
         List<Node> tags = body.list(".tag-list");
         boolean status = false;
-        for(Node tag:tags){
-            if(tag.text().equals("已完结")||tag.text().equals("已完結")){
+        for (Node tag : tags) {
+            if (tag.text().equals("已完结") || tag.text().equals("已完結")) {
                 status = true;
             }
         }
@@ -108,40 +103,38 @@ public class Baozi extends MangaParser {
     public List<Chapter> parseChapter(String html, Comic comic, Long sourceComic) {
         List<Chapter> list = new LinkedList<>();
         Node body = new Node(html);
-        List<Node>chapterNodes= body.list(".comics-chapters");
-
+        List<Node> chapterNodes = Lists.reverse(body.list(".comics-chapters"));
         int i = 0;
         Set<String> pathSet = new HashSet<>();
-        for(Node chapterNode:chapterNodes){
+        for (Node chapterNode : chapterNodes) {
             String title = chapterNode.text("div > span");
             String path = chapterNode.href("a").split("chapter_slot=")[1];
-            if(pathSet.contains(path)){
+            if (pathSet.contains(path)) {
                 continue;
             }
             pathSet.add(path);
             list.add(new Chapter(Long.parseLong(sourceComic + "0" + i++), sourceComic, title, path));
         }
-
-        return Lists.reverse(list);
+        return list;
     }
 
 
     @Override
     public Request getImagesRequest(String cid, String path) {
-        String url = StringUtils.format("%s/comic/chapter/%s/0_%s.html",baseUrl,cid,path);
+        String url = StringUtils.format("%s/comic/chapter/%s/0_%s.html", baseUrl, cid, path);
         return new Request.Builder().url(url).build();
     }
 
     @Override
-    public List<ImageUrl> parseImages(String html,Chapter chapter) {
+    public List<ImageUrl> parseImages(String html, Chapter chapter) {
         List<ImageUrl> list = new ArrayList<>();
         Node body = new Node(html);
         List<Node> imageNodes = body.list("amp-img > noscript");
-        for(int i=1;i<=imageNodes.size();i++){
+        for (int i = 1; i <= imageNodes.size(); i++) {
             Long comicChapter = chapter.getId();
             Long id = Long.parseLong(comicChapter + "0" + i);
-            String imgUrl = imageNodes.get(i-1).src("img");
-            list.add(new ImageUrl(id,comicChapter,i,imgUrl,false));
+            String imgUrl = imageNodes.get(i - 1).src("img");
+            list.add(new ImageUrl(id, comicChapter, i, imgUrl, false));
         }
 
         return list;
