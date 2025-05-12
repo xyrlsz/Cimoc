ff770af061b43ddbebf4a6a6b0ff21cc55746d99
xyrlsz <xyrlsz@outlook.com>
2025-05-12 17:50:07 +0800

    🐞 fix: 修复MYCOMIC的搜索

--- app/src/main/java/com/xyrlsz/xcimoc/source/MYCOMIC.java
+++ app/src/main/java/com/xyrlsz/xcimoc/source/MYCOMIC.java
@@ -50,13 +50,16 @@ public class MYCOMIC extends MangaParser {
             return null;
         }
         String url = baseUrl + "/comics?q=" + keyword + "&page=" + page;
-        return new Request.Builder().url(url).build();
+        return new Request.Builder()
+                .url(url)
+                .addHeader("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36 Edg/136.0.0.0")
+                .build();
     }
 
     @Override
     public SearchIterator getSearchIterator(String html, int page) {
         Node body = new Node(html);
-        return new NodeIterator(body.list(".grow > div > .group")) {
+        return new NodeIterator(body.list(".grid > .group")) {
             @Override
             protected Comic parse(Node node) {
                 String title = node.text("[data-flux-subheading]");
