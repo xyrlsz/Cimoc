3e1beaa6dde704c095a8630812afffc2c025d29f
xyrlsz <xyrlsz@outlook.com>
2025-06-13 16:47:45 +0800

    🐞 fix: 修复createChapterId和createImageId方法中null参数处理逻辑

--- app/src/main/java/com/xyrlsz/xcimoc/utils/IdCreator.java
+++ app/src/main/java/com/xyrlsz/xcimoc/utils/IdCreator.java
@@ -5,10 +5,12 @@ import com.xyrlsz.xcimoc.model.Comic;
 public class IdCreator {
 
     public static long createChapterId(Long sourceComic, int num) {
+        sourceComic = sourceComic == null? 0 : sourceComic;
         return (sourceComic << 16) | (num & 0xFFFF);
     }
 
     public static long createImageId(Long chapterId, int num) {
+        chapterId = chapterId == null? 0 : chapterId;
         return (chapterId << 16) | (num & 0xFFFF);
     }
 
