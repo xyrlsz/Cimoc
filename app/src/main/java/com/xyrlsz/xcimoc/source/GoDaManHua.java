3fbde6ba0263d9e68246a4ab853759b9782d51e9
xyrlsz <xyrlsz@outlook.com>
2025-08-15 23:20:38 +0800

    🐞 fix: 更新G社漫畫链接

--- app/src/main/java/com/xyrlsz/xcimoc/source/GoDaManHua.java
+++ app/src/main/java/com/xyrlsz/xcimoc/source/GoDaManHua.java
@@ -39,8 +39,8 @@ public class GoDaManHua extends MangaParser {
     public static final int TYPE = 108;
     public static final String DEFAULT_TITLE = "G社漫畫";
     private static final String baseUrl = "https://m.g-mh.org";
-    private static final String picBaseUrl = "https://f40-1-4.g-mh.online";
-    private static final String apiBaseUrl = "https://api-get-v2.mgsearcher.com";
+    private static final String picBaseUrl = "https://t40-1-4.g-mh.online";
+    private static final String apiBaseUrl = "https://api-get-v3.mgsearcher.com";
     private String _mid = "";
 
     public GoDaManHua(Source source) {
